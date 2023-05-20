a38e548677f72b877727622367d29464f8373d30
MWTJC <74529082+MWTJC@users.noreply.github.com>
2023-05-21 04:10:36 +0800

    对比添加疑似v6

--- src/network.h
+++ src/network.h
@@ -6,6 +6,8 @@
 
 #include <tuple>
 
+#include <boost/asio.hpp>
+
 #include <enet/enet.h>
 
 #include "utility.h"
@@ -23,6 +25,11 @@ namespace net {
     LAN,
     WAN
   };
+  
+  enum af_e : int {
+    IPV4,
+    BOTH
+  };
 
   net_e
   from_enum_string(const std::string_view &view);
@@ -31,7 +38,14 @@ namespace net {
 
   net_e
   from_address(const std::string_view &view);
+  
+  af_e af_from_enum_string(const std::string_view &view);
+  std::string_view af_to_any_address_string(af_e af);
+  std::string_view af_to_enum_string(af_e af);
 
   host_t
-  host_create(ENetAddress &addr, std::size_t peers, std::uint16_t port);
+  host_create(af_e af, ENetAddress &addr, std::size_t peers, std::uint16_t port);
+
+  std::string addr_to_normalized_string(boost::asio::ip::address address);
+  std::string addr_to_url_escaped_string(boost::asio::ip::address address);
 }  // namespace net
